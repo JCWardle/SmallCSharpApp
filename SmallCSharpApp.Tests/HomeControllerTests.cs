00a8503eed46eb7a792fa85c1f7bc099e4754bd2
Jordan Wardle <jordan@ninjatertle.com>
2016-05-16 13:47:03 +1000

    Added tests.

---
+++ SmallCSharpApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmallCSharpApp.Controllers;
+using SmallCSharpApp.Models;
+using System.Web.Mvc;
+
+namespace SmallCSharpApp.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void CalculatesProduct()
+        {
+            var controller = new HomeController();
+
+            var result = (ViewResult)controller.Calculate(15, 5);
+            var model = (CalculateViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(75, model.Product);
+        }
+
+        [TestMethod]
+        public void CalculatesSum()
+        {
+            var controller = new HomeController();
+
+            var result = (ViewResult)controller.Calculate(35, 5);
+            var model = (CalculateViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(40, model.Sum);
+        }
+
+        [TestMethod]
+        public void CalculatesDifference()
+        {
+            var controller = new HomeController();
+
+            var result = (ViewResult)controller.Calculate(25, 2);
+            var model = (CalculateViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(23, model.Difference);
+        }
+
+        [TestMethod]
+        public void CalculatesQuotient()
+        {
+            var controller = new HomeController();
+
+            var result = (ViewResult)controller.Calculate(40, 10);
+            var model = (CalculateViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(4, model.Quotient);
+        }
+    }
+}
